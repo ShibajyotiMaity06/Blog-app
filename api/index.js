cddd1198a740d6876e5c04d5ec8a808bba7e8640
deba-maity <debajyoti.maity29@gmail.com>
2024-06-29 20:00:58 +0530

    mongo connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGOURL).then(()=>{
+    console.log('connect hogaya re baba')
+}).catch((err) =>{
+    console.log(err)
+})
 const app = express();
 
 app.listen(3000,() => {
