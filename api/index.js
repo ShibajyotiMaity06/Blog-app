479271d265b48b49d139235566dfb880a423c3d2
deba-maity <debajyoti.maity29@gmail.com>
2024-06-30 01:21:50 +0530

    signup api created and tested also hashed password

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -14,8 +15,10 @@ mongoose.connect(process.env.MONGOURL).then(()=>{
 })
 const app = express();
 
+app.use(express.json());
 app.listen(3000,() => {
     console.log('chal raha bhai 3k mei')
 })
 
 app.use('/api/user' , userRoutes);
+app.use('/api/auth' , authRoutes);
